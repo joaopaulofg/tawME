bafb6ee09ef080bc7a080cd184af74f1f9023c42
joaopaulofg <joaofpaulo1@gmail.com>
2025-05-24 20:47:42 -0300

    subindo estrutura inicial do ms de usuarios

---
+++ user-service/src/test/java/com/tawme/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawme.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
