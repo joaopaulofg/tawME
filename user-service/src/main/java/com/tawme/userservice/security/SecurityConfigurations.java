c9c154940dcc467fcbaca95ef0d0f1f521a5d52e
joaopaulofg <joaofpaulo1@gmail.com>
2025-05-25 10:06:58 -0300

    adicionando configuracoes iniciais de seguranca

--- user-service/src/main/java/com/tawme/userservice/security/SecurityConfigurations.java
+++ user-service/src/main/java/com/tawme/userservice/security/SecurityConfigurations.java
@@ -3,14 +3,39 @@ package com.tawme.userservice.security;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @AllArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
